9b3dc50b4f4ea1b4444ffa564c4112f59fe6c85f
Murilo Polese <murilopolese@gmail.com>
2020-04-07 01:19:04 +0100

    Improve mouse interaction

--- spike.js
+++ spike.js
@@ -1,7 +1,7 @@
 function Spike(points, index) {
 	this.index = index
 	this.distance = 250
-	this.maxAngle = 20
+	this.maxAngle = 15
 	this.angle = 0
 	this.size = 1
 	this.spike = createVector(
@@ -20,11 +20,17 @@ function Spike(points, index) {
 		let d = map(
 			min(dist(cursor.x, cursor.y, this.axis.x, this.axis.y), this.distance),
 			0, this.distance,
-			1, 0
+			1, 0.05
 		)
-		this.angle = d * (
-			( this.maxAngle/2 ) + ( this.maxAngle * sin( (this.index*10) + millis()*2 ) )
-		) * 0.5
+		if (d > 0.7) {
+			this.angle =
+			d * (
+				( this.maxAngle/2 ) + ( this.maxAngle * sin( (this.index*40) + millis() ) )
+			)
+		} else {
+			this.angle = ( this.maxAngle/2 ) + ( this.maxAngle * sin( (this.index*40) + millis()/8 ) )
+		}
+
 		this.size = map(d, 0, 1, 0.9, 2.5)
 	}
 
